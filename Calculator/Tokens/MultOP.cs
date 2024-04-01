dedc9453d67a4df361c83b09ce149fbd4d45e6f8
Eddie Olofsgård <eddieolofsgard@icloud.com>
2024-04-01 16:12:41 +0200

    Renamed token.cs to Token.cs Added abstract method ToString() to Token class Added Token subclasses: MultOP SubOP Num
     On branch Backend
     Your branch is up to date with 'origin/Backend'.
    
     Changes to be committed:
    	new file:   Calculator/Tokens/MultOP.cs
    	new file:   Calculator/Tokens/Num.cs
    	new file:   Calculator/Tokens/SubOP.cs
    	new file:   Calculator/Tokens/Token.cs
    	deleted:    Calculator/Tokens/token.cs

---
+++ Calculator/Tokens/MultOP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tokens
+{
+    public class MultOP : Token
+    {
+        public MultOP() : base(1) { }
+
+        public override double eval(Stack<Token> inputStack)
+        {
+            double eval1 = inputStack.Pop().eval(inputStack);
+            double eval2 = inputStack.Pop().eval(inputStack);
+            return eval2 * eval1;
+        }
+
+        public override string ToString()
+        {
+            return "*";
+        }
+    }
+}
---
+++ Calculator/Tokens/Num.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tokens
+{
+    public class Num : Token
+    {
+        public double Value { get; set; }
+
+        public Num(double input) : base(-1) {
+            this.Value = input;
+        }
+
+        public override double eval(Stack<Token> inputStack)
+        {
+            return this.Value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
