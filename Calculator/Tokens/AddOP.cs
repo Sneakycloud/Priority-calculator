5002d0c9019b90b6b9336eb1931ba9b8d5868c3c
Sara Odqvist <saraodqvist@outlook.com>
2024-04-03 09:51:33 +0200

    Added comments

--- Calculator/Tokens/AddOP.cs
+++ Calculator/Tokens/AddOP.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// namespace declaration
 namespace Calculator.Tokens
 {
-    public class AddOP : Token
+    // class declaration
+    // the class AddOP inherits from the base class Token
+    public class AddOP : Token          
     {
+
+        // the constructor with priority order 0
         public AddOP() : base(0) { }
 
+
+        // method to evaluate the add operation
         public override double eval(Stack<Token> inputStack)
         {
+            // pop the first and second token from the inputStack and evaluate them recursively
             double eval1 = inputStack.Pop().eval(inputStack);
             double eval2 = inputStack.Pop().eval(inputStack);
+
+            // return the sum of the two evaluated tokens
             return eval1 + eval2;
         }
 
+        // override to return a string representation of the add token
         public override string ToString()
         {
             return "+";
