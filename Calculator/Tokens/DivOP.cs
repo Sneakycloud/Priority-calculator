c65867ab8fcef95de597a5bcd72a215b55405ecc
Sara Odqvist <saraodqvist@outlook.com>
2024-04-03 10:56:01 +0200

    Added comments to DivOP

--- Calculator/Tokens/DivOP.cs
+++ Calculator/Tokens/DivOP.cs
@@ -8,15 +8,21 @@ namespace Calculator.Tokens
 {
     public class DivOP : Token
     {
+        // the constructor DivOP with priority order 1
         public DivOP() : base(1) { }
 
         public override double eval(Stack<Token> inputStack)
         {
+            // pop the first and second tokens from the inputStack and evaluate them recursively
             double eval1 = inputStack.Pop().eval(inputStack);
             double eval2 = inputStack.Pop().eval(inputStack);
+
+            // return the quota of the two evaluated tokens
             return eval1 / eval2;
         }
 
+
+        // override to return a string representation of the div token
         public override string ToString()
         {
             return "/";
