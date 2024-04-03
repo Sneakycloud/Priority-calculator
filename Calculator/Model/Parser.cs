c07d1adfeef6bb6b21051e7aea60e8e7fc960613
Sara Odqvist <saraodqvist@outlook.com>
2024-04-03 11:39:58 +0200

    Added code to Parser

--- Calculator/Model/Parser.cs
+++ Calculator/Model/Parser.cs
@@ -12,5 +12,28 @@ namespace Calculator.Model
         // Example:
         // Input: ”2+10”
         // Parser.parse(”2+10”) = [”2”,”+”,”10”] = parsedResult         //where parsedResult is the string queue
+
+
+        public static Queue<string> ParseExpression(string inputExpression)
+        {            
+            
+            // parsedResult is the queue that will store the split string
+            Queue<string> parsedResult = new Queue<string>();
+
+
+            // split the inputExpression string into individual tokens (words) and add them to the input string.
+            string[] input = inputExpression.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            // enqueue the tokens into the parsedResult queue
+            foreach (string token in input)
+            {
+                parsedResult.Enqueue(token);
+            }
+
+
+            // return the parsedResult string queue
+            return parsedResult;
+        }
     }
 }
