05ebb214b2460d062c152f7b27046fef6124c3c2
Sara Odqvist <saraodqvist@outlook.com>
2024-04-03 09:46:11 +0200

    Added an add subclass

--- Calculator/Tokens/AddOP.cs
+++ Calculator/Tokens/AddOP.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Calculator.Tokens
 {
-    internal class AddOP
+    public class AddOP : Token
     {
+        public AddOP() : base(0) { }
+
+        public override double eval(Stack<Token> inputStack)
+        {
+            double eval1 = inputStack.Pop().eval(inputStack);
+            double eval2 = inputStack.Pop().eval(inputStack);
+            return eval1 + eval2;
+        }
+
+        public override string ToString()
+        {
+            return "+";
+        }
     }
 }
