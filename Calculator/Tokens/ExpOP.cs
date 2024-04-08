e03b8c1b5f6dd47789d9abc8d55d4ce8390f04b1
Eddie Olofsgård <eddieolofsgard@icloud.com>
2024-04-08 15:35:55 +0200

    Fixes #13. Adds in ExpOP, LeftParentheses and RightParentheses. Also adds these classes into the TokenFactory.
    Changes to be committed:
    	new file:   Calculator/Tokens/ExpOP.cs
    	new file:   Calculator/Tokens/LeftParentheses.cs
    	new file:   Calculator/Tokens/RightParentheses.cs
    	modified:   Calculator/Tokens/TokenFactory.cs

---
+++ Calculator/Tokens/ExpOP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tokens
+{
+    public class ExpOP : Token
+    {
+        public ExpOP() : base(2) { }
+
+        public override double eval(Stack<Token> inputStack)
+        {
+            (double, double) values = getValues(inputStack);
+            return Math.Pow(values.Item2, values.Item1);
+        }
+
+        public override string ToString()
+        {
+            return "^";
+        }
+    }
+}
---
+++ Calculator/Tokens/RightParentheses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tokens
+{
+    public class RightParentheses : Token
+    {
+        public RightParentheses() : base(3) { }
+
+        //This is only called if something has gone wrong, i.e the expression is not valid
+        public override double eval(Stack<Token> inputStack)
+        {
+            throw new Exception();
+        }
+        public override string ToString()
+        {
+            return ")";
+        }
+    }
+}
