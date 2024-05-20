8879b89712e73ce7c2f1e434106507091a7f61ea
Eddie Olofsgård <eddieolofsgard@icloud.com>
2024-05-20 21:01:42 +0200

    Fixed bug parser bug with only two parts Before this commit functions like 5! which only consists of a 5 and a ! failed

--- Calculator/Model/Parser.cs
+++ Calculator/Model/Parser.cs
@@ -90,8 +90,13 @@ namespace Calculator.Model
                 cleaned_commas.RemoveRange(0, 2);
                 cleaned_commas.Insert(0, new_index0);
             }
+            //Handles if the count is only 2
+            else if(cleaned_commas.Count == 2)
+            {
+                parsedResult.Enqueue(cleaned_commas[0]);
+            }
 
-            //Allows for negative numbers to inputed. Like 2+-1 or 2*-(π)
+            //Allows for negative numbers to inputed. Like 2+-1 or 2*-(π) or 3*(-2)
             bool addLast = false;
             for (int i = 1;i < cleaned_commas.Count() - 1; i++)
             {
