8e859bb6d387c72015ca5bbfb8f6c61ccca62956
Eddie Olofsgård <eddieolofsgard@icloud.com>
2024-05-23 22:47:21 +0200

    Removes current implementation of History This is as it causes problems with the dotnet publish command causing all generated .exe to be unusable.
    This reverts commit 445712bdf242ad0b6bc191c36e3b5ebf8d1efbcf, reversing
    changes made to 2386484f10682f37bfc54d50af24ac1338a0aa6f.

--- Calculator/View/MainWindow.xaml.cs
+++ Calculator/View/MainWindow.xaml.cs
@@ -17,8 +17,6 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Linq.Expressions;
 using System.Windows.Media.Animation;
-using System.IO;
-using System.Diagnostics;
 
 namespace Calculator.View
 {
@@ -28,23 +26,11 @@ namespace Calculator.View
     public partial class MainWindow : Window
     {
         private viewModel _vm = new viewModel();
-        private string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "View", "History.txt"));
 
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = _vm;
-            // Delete content in History.txt
-            File.WriteAllText(path, String.Empty);
-        }
-
-        public MainWindow(bool testing)
-        {
-            InitializeComponent();
-            this.DataContext = _vm;
-            // Delete content in History.txt
-            if(testing) path = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "..", "Calculator", "View", "History.txt")); ;
-            File.WriteAllText(path, String.Empty);
         }
 
         public void num_Click(object sender, RoutedEventArgs e)
@@ -78,23 +64,12 @@ namespace Calculator.View
         {
             // calculate and output the result
             viewModel vm = (viewModel)this.DataContext;
-
-            // Path to History.txt
-            string fullPath = path;
-
-            string ex = _vm.Expression + " = ";
-
+            
             if (rm.IsChecked == true)
                 vm.Calculator();
             else
                 vm.RPN_Calculator();
 
-            ex += _vm.Expression;
-            // Add to History.txt
-            using (StreamWriter sw = File.AppendText(fullPath))
-            {
-                sw.WriteLine(ex);
-            }
         }
 
         private void op_Click(object sender, RoutedEventArgs e)
@@ -193,10 +168,6 @@ namespace Calculator.View
                 Title = "RPN Calculator";
             }
         }
-        private void History_Click(object sender, RoutedEventArgs e)
-        {
-            // Open history in notepad
-            Process.Start("notepad.exe", path);
-        }
+
     }
 }
